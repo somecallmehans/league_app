ca1f9bb93d995e94249c5655c722118731b7b9cb
christo <somecallmehans@gmail.com>
2024-12-02 08:31:10 -0500

    update tailwind config

--- tabernacle/tailwind.config.js
+++ tabernacle/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  important: true,
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
