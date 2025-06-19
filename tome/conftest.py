7671fc0a5946bafa7cd419056b7139d038394161
christo <somecallmehans@gmail.com>
2025-06-19 19:36:53 -0400

    load csv data in

--- tome/conftest.py
+++ tome/conftest.py
@@ -1,55 +1,11 @@
 import pytest
 from rest_framework.test import APIClient
 from django.db import connection
+from pathlib import Path
 
-from users.models import Participants, Users
-from sessions_rounds.models import Sessions, Rounds
-from achievements.models import Achievements
+from users.models import Users
 
-test_participants = [
-    Participants(id=901, name="Charlie Smith"),
-    Participants(id=902, name="Trenna Thain"),
-    Participants(id=903, name="Fern Penvarden"),
-    Participants(id=904, name="Nikita Heape"),
-    Participants(id=905, name="Bevon Goldster"),
-    Participants(id=906, name="Jeffrey Blackwood"),
-    Participants(id=907, name="Amanda Tinnin"),
-    Participants(id=908, name="Bless Frankfurt"),
-    Participants(id=909, name="Fran Brek"),
-    Participants(id=910, name="Thom Horn"),
-]
-
-test_sessions = [
-    Sessions(id=101, month_year="10-24", closed=True, created_at=1729900800),
-    Sessions(id=102, month_year="11-24", closed=True, created_at=1730592000),
-    Sessions(id=103, month_year="11-24", closed=False, created_at=1731196800),
-]
-
-test_rounds = [
-    Rounds(id=111, session_id=101, round_number=1),
-    Rounds(id=112, session_id=101, round_number=2),
-    Rounds(id=113, session_id=102, round_number=1),
-    Rounds(id=114, session_id=102, round_number=2),
-    Rounds(id=115, session_id=103, round_number=1),
-    Rounds(id=116, session_id=103, round_number=2),
-]
-
-test_achievements = [
-    Achievements(id=24, name="Participation", slug="participation", point_value=3),
-    Achievements(id=25, name="Kill the table", point_value=2),
-    Achievements(
-        id=26, name="Win with a deck that has no instants or sorceries", point_value=5
-    ),
-    Achievements(
-        id=27, name="Win with no creatures except your commander", point_value=4
-    ),
-    Achievements(id=28, name="Win with no lands", point_value=9),
-    Achievements(id=29, name="Win with 88 or more basic lands", point_value=11),
-    Achievements(
-        id=30, name="Win via commander damage", slug="cmdr-damage", point_value=1
-    ),
-    Achievements(id=31, name="The game is a draw", slug="end-draw", point_value=3),
-]
+SEED_DIRECTORY = "./test_db_seeds"
 
 
 @pytest.fixture
@@ -77,12 +33,18 @@ def client(api_client, mock_authenticated_user):
 
 @pytest.fixture(autouse=True, scope="function")
 def seed_db(db):
-    Participants.objects.bulk_create(test_participants)
-    Sessions.objects.bulk_create(test_sessions)
-    Rounds.objects.bulk_create(test_rounds)
-    Achievements.objects.bulk_create(test_achievements)
-
     with connection.cursor() as cursor:
+        for file in sorted(Path(SEED_DIRECTORY).iterdir()):
+            with file.open() as f:
+                colnames, *_ = f.readlines()
+                colnames = colnames.strip()
+                _, table_name = file.stem.split("_", maxsplit=1)
+
+                f.seek(0)
+                cursor.copy_expert(
+                    f"COPY {table_name} ({colnames}) FROM STDIN CSV HEADER", f
+                )
+
         cursor.execute(
             """
             SELECT setval('participants_id_seq', (SELECT MAX(id) FROM participants));
--- tome/utils/test_helpers.py
+++ tome/utils/test_helpers.py
@@ -1,3 +1,12 @@
+from typing import Any
+
+from csv import reader
+from pathlib import Path
+from functools import cache
+from dataclasses import dataclass, field
+from conftest import SEED_DIRECTORY
+
+
 def prune_fields(data, fields_to_keep):
     """
     Recursively remove all keys from nested dictionaries that are not in `fields_to_keep`.
@@ -12,3 +21,58 @@ def prune_fields(data, fields_to_keep):
     elif isinstance(data, list):
         return [prune_fields(item, fields_to_keep) for item in data]
     return data
+
+
+def id_from_csv_row(csv_path: str, index: int) -> Any:
+    path = SEED_DIRECTORY / Path(f"{csv_path}.csv")
+    with path.open() as f:
+        seed_reader = reader(f)
+        header = next(seed_reader)
+        id_idx = header.index("id")
+        unique_ids = sorted({int(row[id_idx]) for row in seed_reader})
+    return field(default_factory=lambda: unique_ids[index])
+
+
+@dataclass
+class Identifiers:
+    """
+    Holds the primary keys for testdb records for use
+    in other places.
+    """
+
+    P1: int = id_from_csv_row("100_participants", 0)
+    P2: int = id_from_csv_row("100_participants", 1)
+    P3: int = id_from_csv_row("100_participants", 2)
+    P4: int = id_from_csv_row("100_participants", 3)
+    P5: int = id_from_csv_row("100_participants", 4)
+    P6: int = id_from_csv_row("100_participants", 5)
+    P7: int = id_from_csv_row("100_participants", 6)
+    P8: int = id_from_csv_row("100_participants", 7)
+    P9: int = id_from_csv_row("100_participants", 8)
+    P10: int = id_from_csv_row("100_participants", 9)
+
+    SESSION_LAST_MONTH: int = id_from_csv_row("200_sessions", 0)
+    SESSION_THIS_MONTH_CLOSED: int = id_from_csv_row("200_sessions", 1)
+    SESSION_THIS_MONTH_OPEN: int = id_from_csv_row("200_sessions", 2)
+
+    R1_SESSION_LAST_MONTH: int = id_from_csv_row("250_rounds", 0)
+    R2_SESSION_LAST_MONTH: int = id_from_csv_row("250_rounds", 1)
+    R1_SESSION_THIS_MONTH_CLOSED: int = id_from_csv_row("250_rounds", 2)
+    R2_SESSION_THIS_MONTH_CLOSED: int = id_from_csv_row("250_rounds", 3)
+    R1_SESSION_THIS_MONTH_OPEN: int = id_from_csv_row("250_rounds", 4)
+    R2_SESSION_THIS_MONTH_OPEN: int = id_from_csv_row("250_rounds", 5)
+
+    PARTICIPATION: int = id_from_csv_row("400_achievements", 0)
+    ACHIVEMENT_1: int = id_from_csv_row("400_achievements", 1)
+    ACHIVEMENT_2: int = id_from_csv_row("400_achievements", 2)
+    ACHIVEMENT_3: int = id_from_csv_row("400_achievements", 3)
+    ACHIVEMENT_4: int = id_from_csv_row("400_achievements", 4)
+    ACHIVEMENT_5: int = id_from_csv_row("400_achievements", 5)
+    ACHIVEMENT_6: int = id_from_csv_row("400_achievements", 6)
+    ACHIVEMENT_7: int = id_from_csv_row("400_achievements", 7)
+
+
+@cache
+def get_ids() -> Identifiers:
+    """Return the singleton of identifiers"""
+    return Identifiers()
