78c6c0c6ee4702958d23e048f41e65c3f46a5862
christo <somecallmehans@gmail.com>
2025-09-24 09:59:19 -0400

    working bot

--- bot/src/main.py
+++ bot/src/main.py
@@ -1,6 +1,12 @@
 import os, json
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env", override=False)
+
 from fastapi import FastAPI, Request, HTTPException
 from .verify import verify_signature
+
 
 app = FastAPI()
 
@@ -15,12 +21,11 @@ APP_COMMAND_AUTOCOMPLETE = 4
 MODAL_SUBMIT = 5
 
 
-@app.get("/interactions")
+@app.post("/interactions")
 async def interactions(req: Request):
     sig = req.headers.get(SIGNATURE)
     ts = req.headers.get(TIMESTAMP)
     body = await req.body()
-    print(os.getenv("DISCORD_PUBLIC_KEY"))
 
     if not sig or not ts or not verify_signature(sig, ts, body):
         raise HTTPException(status_code=401, detail="Invalid signature")
@@ -28,5 +33,9 @@ async def interactions(req: Request):
     payload = json.loads(body)
     t = payload["type"]
 
+    print("HIT!")
+
     if t == PING:
         return {"type": 1}
+
+    return {"type": 4, "data": {"flags": 64, "content": "Hello world!!!!"}}
