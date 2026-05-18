c6c4cd80530aa250021be1877883b88937058fe1
christo <somecallmehans@gmail.com>
2026-05-17 21:38:29 -0400

    maybe fix button mash

--- tabernacle/src/components/Modal.tsx
+++ tabernacle/src/components/Modal.tsx
@@ -38,7 +38,7 @@ export default function Modal({
           enter="ease-out duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-0"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
@@ -51,7 +51,7 @@ export default function Modal({
               enter="ease-out duration-300"
               enterFrom="opacity-0 scale-95"
               enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
+              leave="ease-in duration-0"
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
